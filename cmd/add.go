43cd2a3eb6e65bf8accbe8aa7419afcfba99c1c0
Makussu <max.brochier1@gmail.com>
2022-07-10 18:46:02 +0200

    update : update add.go file with detailed description

--- cmd/add.go
+++ cmd/add.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Brochier Maxence maxence@brochier.xyz
 
 */
 package cmd
@@ -13,7 +13,9 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new todo",
-	Long: `Add a new todo`,
+	Long: `Add a new todo
+To add an entry,
+Do gotodo add <text to describe your todo>`,
 	Run: func(cmd *cobra.Command, args []string) {
 		execution.AddTodo()
 	},
@@ -21,14 +23,4 @@ var addCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
